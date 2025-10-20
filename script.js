114209316fde737564bc8618b971b650202793e4
PARKOHSAM <ajaoadediran@gmail.com>
2025-10-20 16:42:35 +0100

    refactor: improve grade checking logic and enhance CSS styling

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const checkGrade = () => {
-    if (grading.value == '') {
+    if (grading.value === '') {
         showResult.innerHTML = 'Fill in your CGPA 🙄'
     }
-    if (grading.value >= 4.50 || grading.value <= 5.0) {
+    else if (grading.value > 5.0){
+        showResult.innerHTML = "value can't be higher than 5.0"
+    }
+    else if (grading.value >= 4.50 && grading.value <= 5.0) {
         showResult.innerHTML = "First Class🥰"
     }
-    if (grading.value == 3.50 || grading.value <= 4.49) {
+    else if (grading.value >= 3.50 && grading.value <= 4.49) {
         showResult.innerHTML = "Second Class Upper 😍"
-    } if (grading.value == 2.40 || grading.value <= 3.49) {
+    } else if (grading.value >= 2.40 && grading.value <= 3.49) {
         showResult.innerHTML = "Second Class Lower 🤗"
-    } if (grading.value == 1.50 || grading.value <= 2.39) {
+    } else if (grading.value >= 1.50 && grading.value <= 2.39) {
         showResult.innerHTML = "Third Class 🙂"
     } else {
-        showResult.innerHTML = "Enter valid CGPA🙄"
+        showResult.innerHTML = "Pass🙄"
     }
 }
